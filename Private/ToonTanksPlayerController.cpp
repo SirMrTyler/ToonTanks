a5ae5188197807fb4f759d08480b9f9d5b77d389
SirMrTyler <110429269+SirMrTyler@users.noreply.github.com>
2022-10-06 14:42:24 -0700

    Update ToonTanksPlayerController.cpp

--- Private/ToonTanksPlayerController.cpp
+++ Private/ToonTanksPlayerController.cpp
@@ -4,13 +4,17 @@
 #include "ToonTanksPlayerController.h"
 #include "GameFramework/Pawn.h"
 
+// This tells the system when the tank can be controlled by the user depending on whether bPlayerEnabled is true or not
 void AToonTanksPlayerController::SetPlayerEnabledState(bool bPlayerEnabled) {
-    // This tells the system when the tank can be controlled by the user depending on whether bPlayerEnabled is true or not
-    if(bPlayerEnabled) {
+    if(bPlayerEnabled) 
+    {
         GetPawn()->EnableInput(this);
     }
-    else {
+    else 
+    {
         GetPawn()->DisableInput(this);
     }
+    
+    // The cursor will only show up if the tank is allowed to be controlled by the user
     bShowMouseCursor = bPlayerEnabled;
 }
