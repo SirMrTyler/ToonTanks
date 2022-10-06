f96a71cbcef8fd0ea18880c2de2d8fa936e9d13d
SirMrTyler <110429269+SirMrTyler@users.noreply.github.com>
2022-10-06 14:21:18 -0700

    Update Projectile.cpp

--- Private/Projectile.cpp
+++ Private/Projectile.cpp
@@ -13,16 +13,19 @@
 // Sets default values
 AProjectile::AProjectile()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	// This allows the actor the ability to be updated every frame when set to true.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Here we set our Projectile's static mesh as the parent component of the actor
 	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
 	RootComponent = ProjectileMesh;
 
+	// Here we create the object and give it default speed values
 	ProjectileMoverComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Mover"));
 	ProjectileMoverComp->MaxSpeed = 1500.f;
 	ProjectileMoverComp->InitialSpeed = 1500.f;
 
+	// This section attaches particle effects to our ProjectileComponent
 	ParticleSysComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Smoke Trail"));
 	ParticleSysComponent->SetupAttachment(RootComponent);
 
@@ -48,17 +51,20 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {
 
-	
+	// Pretty straight forward, this plays a sound at the location of the projectiles actor where it collides with something.
 	UGameplayStatics::PlaySoundAtLocation(this, LaunchSound, GetActorLocation());
+	// This allows us to make sure the projectile fired by the user doesn't hit that same user.
 	auto MyOwner = GetOwner();
 	if(MyOwner == nullptr) {
 		Destroy();
 		return;
 	}
 	
+	// This returns the controller (user) of the person who initiated that projectile launch.
 	auto MyOwnerInstigator = MyOwner->GetInstigatorController();
 	auto DamageTypeClass = UDamageType::StaticClass();
-
+	
+	// This states that if the actor hit by missle isn't the person who launched the projectile then damage will be dealt to that actor.
 	if(OtherActor && OtherActor != this && OtherActor != MyOwner) {
 		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwnerInstigator, this, DamageTypeClass);
 		if(HitParticles) {
