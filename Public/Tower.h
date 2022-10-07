6fe6abec8f014b5a1a19a90df9ae2a4bedde1eb4
SirMrTyler <110429269+SirMrTyler@users.noreply.github.com>
2022-10-06 23:35:46 -0700

    Update Tower.h

--- Public/Tower.h
+++ Public/Tower.h
@@ -24,15 +24,21 @@ protected:
 	virtual void BeginPlay() override;
 
 private:
-	
 	class ATank* Tank;
 
+	// Will be used as a comparison value between the tank and the tower telling the tower when it's allowed to shoot at the tank.
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 800.f;
 
+	// Timer is used to declare add a delay between each shot the turret takes.
 	FTimerHandle FireRateTimerHandle;
+	
+	// This is plugged into our timer as a conditional amount of time that must be waited for between each shot.
 	float FireRate = 2.f;
+	
+	// Checks fire range, time between shots, and visibility of tank relative to the tower
 	void CheckFireCondition();
 
+	// Is tank in fire range = true; else = false
 	bool InFireRange();
 };
